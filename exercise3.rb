7e28657357d2dd405f6ac16db3a3902adbde6090
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-08-10 17:02:01 -0400

    final changes

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,44 @@
 puts "What's your name?"
+
+if 2 > 1
+  puts "I get printed!"
+end
+
+number = 3
+
+if number > 0
+  puts "#{number} is positive"
+else
+  puts "#{number} is negative"
+end
+
+if 0 < 1
+  puts "I will not get printed!"
+end
+
+number = -4
+
+if number > 0
+  puts "#{number} is positive"
+else
+  puts "#{number} is negative"
+end
+
+x = 3
+y = 4
+
+if x > y
+  puts "x is greater than y!"
+elsif x < y
+  puts "x is less than y!"
+else
+  puts "x equals y!"
+end
+
+if x !=10
+  puts "I get printed!"
+end
+
+unless x == 10
+  puts "I get printed!"
+end
